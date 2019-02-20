85c19cc121e89336bf611893b63a2ca56c08b60b
Sebastian Webber <sebastian@swebber.me>
2019-02-19 23:48:17 -0300

    fix lineno issue

--- cmd/ui/rules_test.go
+++ cmd/ui/rules_test.go
@@ -11,36 +11,36 @@ import (
 )
 
 var (
-	localRule    = hba.Rule{Type: "local", DatabaseName: "all", UserName: "all", AuthMethod: "trust", Comments: "comment goes here"}
+	localRule    = hba.Rule{LineNumber: 1, Type: "local", DatabaseName: "all", UserName: "all", AuthMethod: "trust", Comments: "comment goes here"}
 	localRuleStr = `
-+-------+----------+------+-----------+--------+-------------------+
-| TYPE  | DATABASE | USER | ADDRESSES | METHOD |     COMMENTS      |
-+-------+----------+------+-----------+--------+-------------------+
-| local | all      | all  |           | trust  | comment goes here |
-+-------+----------+------+-----------+--------+-------------------+
++------+-------+----------+------+-----------+--------+-------------------+
+| LINE | TYPE  | DATABASE | USER | ADDRESSES | METHOD |     COMMENTS      |
++------+-------+----------+------+-----------+--------+-------------------+
+|    1 | local | all      | all  |           | trust  | comment goes here |
++------+-------+----------+------+-----------+--------+-------------------+
 `
 
 	ip, netmask, _ = net.ParseCIDR("192.168.150.0/22")
-	hostRuleCIDR   = hba.Rule{Type: "host", DatabaseName: "all", UserName: "all", IPAddress: ip, NetworkMask: &netmask.Mask, AuthMethod: "trust", Comments: "comment goes here"}
+	hostRuleCIDR   = hba.Rule{LineNumber: 2, Type: "host", DatabaseName: "all", UserName: "all", IPAddress: ip, NetworkMask: &netmask.Mask, AuthMethod: "trust", Comments: "comment goes here"}
 
-	hostRuleDNS = hba.Rule{Type: "host", DatabaseName: "all", UserName: "all", DNSAddress: "super-site.com", AuthMethod: "md5", Comments: "comment goes here"}
+	hostRuleDNS = hba.Rule{LineNumber: 3, Type: "host", DatabaseName: "all", UserName: "all", DNSAddress: "super-site.com", AuthMethod: "md5", Comments: "comment goes here"}
 
 	hostRuleStr = `
-+------+----------+------+------------------+--------+-------------------+
-| TYPE | DATABASE | USER |    ADDRESSES     | METHOD |     COMMENTS      |
-+------+----------+------+------------------+--------+-------------------+
-| host | all      | all  | 192.168.150.0/22 | trust  | comment goes here |
-| host | all      | all  | super-site.com   | md5    | comment goes here |
-+------+----------+------+------------------+--------+-------------------+
++------+------+----------+------+------------------+--------+-------------------+
+| LINE | TYPE | DATABASE | USER |    ADDRESSES     | METHOD |     COMMENTS      |
++------+------+----------+------+------------------+--------+-------------------+
+|    2 | host | all      | all  | 192.168.150.0/22 | trust  | comment goes here |
+|    3 | host | all      | all  | super-site.com   | md5    | comment goes here |
++------+------+----------+------+------------------+--------+-------------------+
 `
 	hostAllStr = `
-+-------+----------+------+------------------+--------+-------------------+
-| TYPE  | DATABASE | USER |    ADDRESSES     | METHOD |     COMMENTS      |
-+-------+----------+------+------------------+--------+-------------------+
-| local | all      | all  |                  | trust  | comment goes here |
-| host  | all      | all  | 192.168.150.0/22 | trust  | comment goes here |
-| host  | all      | all  | super-site.com   | md5    | comment goes here |
-+-------+----------+------+------------------+--------+-------------------+
++------+-------+----------+------+------------------+--------+-------------------+
+| LINE | TYPE  | DATABASE | USER |    ADDRESSES     | METHOD |     COMMENTS      |
++------+-------+----------+------+------------------+--------+-------------------+
+|    1 | local | all      | all  |                  | trust  | comment goes here |
+|    2 | host  | all      | all  | 192.168.150.0/22 | trust  | comment goes here |
+|    3 | host  | all      | all  | super-site.com   | md5    | comment goes here |
++------+-------+----------+------+------------------+--------+-------------------+
 `
 )
 
